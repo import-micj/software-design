9eb9bba46129516d574cc893b313ef7ff5530766
Michael Josh Sarmiento <michaeljsarmiento2k5@gmail.com>
2025-02-08 17:04:58 +0800

    week 3 exercise

--- lab3/Form1.Designer.cs
+++ lab3/Form1.Designer.cs
@@ -20,20 +20,40 @@
 			base.Dispose(disposing);
 		}
 
-		#region Windows Form Designer generated code
+        #region Windows Form Designer generated code
 
-		/// <summary>
-		///  Required method for Designer support - do not modify
-		///  the contents of this method with the code editor.
-		/// </summary>
-		private void InitializeComponent()
-		{
-			this.components = new System.ComponentModel.Container();
-			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-			this.ClientSize = new System.Drawing.Size(800, 450);
-			this.Text = "Form1";
-		}
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            listBox1 = new ListBox();
+            SuspendLayout();
+            // 
+            // listBox1
+            // 
+            listBox1.FormattingEnabled = true;
+            listBox1.ItemHeight = 15;
+            listBox1.Location = new Point(259, 157);
+            listBox1.Name = "listBox1";
+            listBox1.Size = new Size(242, 184);
+            listBox1.TabIndex = 0;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(listBox1);
+            Name = "Form1";
+            Text = "Form1";
+            Load += Form1_Load;
+            ResumeLayout(false);
+        }
 
-		#endregion
-	}
+        #endregion
+
+        private ListBox listBox1;
+    }
 }
--- lab3/Form1.cs
+++ lab3/Form1.cs
@@ -1,10 +1,66 @@
+using System;
+using System.Windows.Forms;
+
 namespace lab3
 {
-	public partial class Form1 : Form
-	{
-		public Form1()
-		{
-			InitializeComponent();
-		}
-	}
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        public class Book
+        {
+            public string Title { get; set; }
+            public string Author { get; set; }
+
+            public virtual string GetInfo()
+            {
+                return $"{Title} by {Author}";
+            }
+        }
+
+        public class Magazine : Book
+        {
+            public int IssueNumber { get; set; }
+
+            public override string GetInfo()
+            {
+                return $"{Title} - Issue {IssueNumber}";
+            }
+        }
+
+        public class Ebook : Book
+        {
+            public string Format { get; set; }
+
+            public override string GetInfo()
+            {
+                return $"{Title} (eBook - {Format})";
+            }
+        }
+
+        // Polymorphic function to process books
+        private void DisplayBooks(Book[] books)
+        {
+            listBox1.Items.Clear(); // Assuming you have a ListBox named listBox1
+            foreach (var book in books)
+            {
+                listBox1.Items.Add(book.GetInfo());
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            Book[] books = new Book[]
+            {
+                new Book { Title = "C# Basics", Author = "John Doe" },
+                new Magazine { Title = "Linus Tech Tips", IssueNumber = 420 },
+                new Ebook { Title = "Learn C# in 10 Hours", Author = "BroCode", Format = "PDF" }
+            };
+
+            DisplayBooks(books);
+        }
+    }
 }
